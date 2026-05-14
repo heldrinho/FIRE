020452671a137389d5459dd0967ff2f4c3e8f0e1
Helder Filipe <heldderfilipe@aluno.unilab.edu.br>
2026-05-14 12:28:58 -0300

    Initial Commit

---
+++ FIRE/FIREApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIREApp.swift
+//  FIRE
+//
+//  Created by Helder Filipe on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FIREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
